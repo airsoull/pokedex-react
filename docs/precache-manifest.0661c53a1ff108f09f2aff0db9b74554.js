8d9ba3bbd57285b084621a0056ed3e12fb257501
Cristian <cristian@magnet.cl>
2020-07-10 18:16:11 -0400

    feat: add docs folder with build project

---
+++ docs/precache-manifest.0661c53a1ff108f09f2aff0db9b74554.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "471b1827b7a2aa02082b8a74c661f6fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f0546de28b0750d0f43",
+    "url": "/static/css/main.ae0eeea0.chunk.css"
+  },
+  {
+    "revision": "0c47177c2299b314715d",
+    "url": "/static/js/2.72a7ec20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.72a7ec20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f0546de28b0750d0f43",
+    "url": "/static/js/main.652a3182.chunk.js"
+  },
+  {
+    "revision": "8b53897bc9935d65a7b5",
+    "url": "/static/js/runtime-main.20a0f47d.js"
+  }
+]);
